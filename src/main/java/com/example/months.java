c57ea29e59da2f36d56feb32edb090096937c357
aj3011 <71029690+aj3011@users.noreply.github.com>
2021-07-17 12:58:41 -0700

    Adding a Controller Class for use in graphs

---
+++ src/main/java/com/example/months.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class months {
+
+  public int start;
+  public int end;
+
+  public void setStart(int s) {
+    this.start = s;
+  }
+
+  public void setEnd(int e) {
+    this.end = e;
+  }
+
+  public int getStart() {
+    return this.start;
+  }
+
+  public int getEnd() {
+    return this.end;
+  }
+
+}
