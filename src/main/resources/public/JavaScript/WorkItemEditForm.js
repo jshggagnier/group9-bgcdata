7efdd637f2dcdc47f9b277ce49595f2366ef60fb
Ronald Mundell <ronniemundell8@gmail.com>
2021-07-18 20:00:11 -0700

    Workedit js

--- src/main/resources/public/JavaScript/WorkItemEditForm.js
+++ src/main/resources/public/JavaScript/WorkItemEditForm.js
@@ -1,4 +1,3 @@
-
 function AddTeam(){
     var table = document.getElementById("InputTable");
     var TableRows = (table.rows.length-1);
@@ -21,55 +20,8 @@ function weeksBetween(StartDate, EndDate) {
     return Math.ceil((EndDate - StartDate) / (7 * 24 * 60 * 60 * 1000));//rounds up the amount of weeks between the two dates (the number is the amount of milliseconds in a week)
 }
 
-function FillTable(){
-var Teams = parseTeamString(document.getElementById("TBstring"));
-var i = 0;
-var TableCells = document.getElementById("Row0").cells.length;
-Teams.forEach(Row => {
-if(i == 0){
-    for(var CellCounter = 0; CellCounter < TableCells; CellCounter++)
-    {
-        if(CellCounter == 0){
-            Row[CellCounter] = Row[CellCounter].slice(3);
-        }
-        document.getElementById("cell"+(i+1)+":"+CellCounter).value = Row[CellCounter];
-    }
-}else{
-    AddTeam();
-        for(var CellCounter = 0; CellCounter < TableCells; CellCounter++)
-        {
-            if(CellCounter == 0){
-                Row[CellCounter] = Row[CellCounter].slice(3);
-            }
-            document.getElementById("cell"+(i+1)+":"+CellCounter).value  = Row[CellCounter];
-        }
-}
-i++;
-});
-}
-
-function parseTeamString(team){
-    var i = 0;
-    team = team.split("|");
-    while(i < team.length){
-    team[i] = team[i].split(",");
-    var Team = "";
-    var c = 0;
-    team[i].forEach(t => {
-        if(c == 0){
-            Team += t.slice(3)+" ";
-            c++;
-        }else{
-        Team += t+" ";
-        }
-    });
-    i++;
-}
-return team;
-}
-
 function UpdateTable(){
-    console.log("Updating Table");
+    //console.log("Updating Table");
     var table = document.getElementById("InputTable");
     var StartDate = document.getElementById("startDate").valueAsDate;
     var EndDate = document.getElementById("endDate").valueAsDate;
@@ -95,7 +47,7 @@ function UpdateTable(){
         }
         while (cellcounter != (weeks+1))
         {
-            console.log(cellcounter + "/" + weeks);
+            //console.log(cellcounter + "/" + weeks);
             if (cellcounter < (weeks+1)) {
                 currentcellpntr = currentrowpntr.insertCell(-1);
                 if(RowCounter == 0)
@@ -103,11 +55,16 @@ function UpdateTable(){
                     var Cheese = new Date(StartDate.getTime() + ((cellcounter-1)*(7 * 24 * 60 * 60 * 1000) + (24 * 60 * 60 * 1000)));
                     //maybe concatenate calculation here
                     currentcellpntr.innerHTML = Cheese.toString().substring(0,15);
+                    currentcellpntr.className = "itemheader";
                 }
                 else
                 {
-                    if (cellcounter == 0) {currentcellpntr.innerHTML = "<input type='text' id='"+("cell"+RowCounter+":"+cellcounter)+"'><br><button type='button' onclick='deleterow("+RowCounter+")'>Delete This Row</button>";}
-                    else {currentcellpntr.innerHTML = "<input type='number' id='"+("cell"+RowCounter+":"+cellcounter)+"'  min='0'>";}
+                    if (cellcounter == 0)
+                    {
+                        currentcellpntr.innerHTML = "<input type='text' id='"+("cell"+RowCounter+":"+cellcounter)+"' required><br><button type='button' class='delbtn' id='btn"+RowCounter+"' onclick='deleterow("+RowCounter+")'>Delete This Row</button>";
+                        currentcellpntr.className = "rowname";
+                    }
+                    else {currentcellpntr.innerHTML = "<input type='number' id='"+("cell"+RowCounter+":"+cellcounter)+"'  min='0' required>";}
                 }
             }
             else if (cellcounter > (weeks+1)) {
@@ -116,6 +73,7 @@ function UpdateTable(){
             cellcounter = currentrowpntr.cells.length;
         }
     }
+    createTeamString();
 }
 
 function createTeamString(){
@@ -123,6 +81,7 @@ function createTeamString(){
     var TableRows = (table.rows.length);
     var TableCells = document.getElementById("Row0").cells.length;
     var TableString = "";
+    //console.log(TableRows+"|I|"+TableCells);
     for(var RowCounter = 1;RowCounter < TableRows;RowCounter++)
     {
         TableString += "T"+RowCounter+":"
@@ -135,24 +94,94 @@ function createTeamString(){
         }
         TableString += "|";
     }
-    console.log(TableString);
+    //console.log(TableString);
     document.getElementById("TBstring").value = TableString;
 }
 
 function deleterow(row)
 {
+    console.log("deleterow"+row);
     var table = document.getElementById("InputTable");
+    var StartDate = document.getElementById("startDate").valueAsDate;
+    var EndDate = document.getElementById("endDate").valueAsDate;
+    if(EndDate < StartDate) 
+    {
+        document.getElementById("endDate").valueAsDate = StartDate;
+    }
+    var weeks = weeksBetween(StartDate,EndDate);
     var TableRows = (table.rows.length-1);
     var target;
+    var cellupdate = 0;
+    var cellupdatepnt;
     if(TableRows == 1){alert("Must have at least one Team");}
     else
     {
         table.deleteRow(row);
         while (row < TableRows)
         {
+            console.log(row+",");
             target = document.getElementById("Row"+(row+1));
+            cellupdatepnt = document.getElementById("btn"+(row+1));
+            cellupdatepnt.setAttribute("onclick", "deleterow("+row+");");
+            console.log(cellupdatepnt.onclick);
+            cellupdatepnt.id = "btn"+row;
+            while(cellupdate < target.cells.length)
+            {
+                cellupdatepnt = document.getElementById("cell"+(row+1)+":"+cellupdate);
+                cellupdatepnt.id = "cell"+row+":"+cellupdate;
+                cellupdate++;
+            }
             target.id = "Row"+row;
             row++;
+            cellupdate = 0;
         }
     }
+    createTeamString();
 }
+
+function FillTable(){
+    var Teams = parseTeamString(document.getElementById("TBstring"));
+    var i = 0;
+    var TableCells = document.getElementById("Row0").cells.length;
+    Teams.forEach(Row => {
+    if(i == 0){
+        for(var CellCounter = 0; CellCounter < TableCells; CellCounter++)
+        {
+            if(CellCounter == 0){
+                Row[CellCounter] = Row[CellCounter].slice(3);
+            }
+            document.getElementById("cell"+(i+1)+":"+CellCounter).value = Row[CellCounter];
+        }
+    }else{
+        AddTeam();
+            for(var CellCounter = 0; CellCounter < TableCells; CellCounter++)
+            {
+                if(CellCounter == 0){
+                    Row[CellCounter] = Row[CellCounter].slice(3);
+                }
+                document.getElementById("cell"+(i+1)+":"+CellCounter).value  = Row[CellCounter];
+            }
+    }
+    i++;
+    });
+    }
+    
+    function parseTeamString(team){
+        var i = 0;
+        team = team.split("|");
+        while(i < team.length){
+        team[i] = team[i].split(",");
+        var Team = "";
+        var c = 0;
+        team[i].forEach(t => {
+            if(c == 0){
+                Team += t.slice(3)+" ";
+                c++;
+            }else{
+            Team += t+" ";
+            }
+        });
+        i++;
+    }
+    return team;
+    }
