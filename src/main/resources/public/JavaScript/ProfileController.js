4b69e5c7173d1949552de4bb7df5f92bbe956dbc
Joshua Gagnier <jgagnier@shaw.ca>
2021-07-18 22:56:33 -0700

    updated CSS for profile controller (and clarified quite a lot/fixed some bugs)

--- src/main/resources/public/JavaScript/ProfileController.js
+++ src/main/resources/public/JavaScript/ProfileController.js
@@ -2,10 +2,11 @@ function setup(email,role)
 {
     document.getElementById("email").value = email;
     document.getElementById("role").value = role;
+    document.getElementById("displaymail").innerHTML = email;
     if(role == "admin") {document.getElementById("admin").checked = true;}
     else if(role == "viewedit") {document.getElementById("ViewEdit").checked = true;}
     else if(role == "viewonly") {document.getElementById("ViewOnly").checked = true;}
-    else {document.getElementById("unauthorized").checked = true;}
+    else {document.getElementById("unverified").checked = true;}
 }
 
 function modifyfields()
@@ -13,5 +14,5 @@ function modifyfields()
     if (document.getElementById("admin").checked == true) {document.getElementById("role").value = "admin"}
     else if(document.getElementById("ViewEdit").checked == true) {document.getElementById("role").value = "viewedit"}
     else if(document.getElementById("ViewOnly").checked == true) {document.getElementById("role").value = "viewonly"}
-    else {document.getElementById("role").value = "unauthorized"}
+    else {document.getElementById("role").value = "unverified"}
 }
